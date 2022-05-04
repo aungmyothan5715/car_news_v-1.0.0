2018b5e5b44579ccf8afeed5e148714792552754
aungmyothan5715 <aungmyothan571@gmail.com>
2022-05-04 20:10:25 +0630

    v-1.0.0

---
+++ js/modal.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#login-btn").click(function(){
+        $("#login-form").show();
+    });
+
+    $("#register-btn").click(function(){
+        $("#register-form").show();
+    });
+});
